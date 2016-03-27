bf8b2e8733a2efdd3f4fa8c9ffa1e39a112fc9e6
TomasHo <tomas.horvath@gmail.com>
2016-03-27 20:33:27 +0200

    Directory structure changes Check for 'Data' & 'Data_processed' dir to exist

---
+++ EKS/EKS.py
@@ -0,0 +1,97 @@
+import json
+import re
+import os, sys
+
+from EKS import smtp
+from datetime import datetime
+from bs4 import BeautifulSoup
+from urllib import request
+from configparser import ConfigParser
+from time import sleep
+
+assert sys.version_info.major >= 3
+if not os.path.isdir('EKS/Data'): os.mkdir('EKS/Data')
+if not os.path.isdir('EKS/Data_processed'): os.mkdir('EKS/Data_processed')
+
+config = ConfigParser()
+config.read("EKS/settings.ini")
+
+try:
+    with open('EKS/list.json') as file:
+        file_list = json.load(file)
+except IOError:
+    for (dirpath, dirnames, filenames) in os.walk('EKS/Data'):
+        file_list = []
+        file_list.extend(filenames)
+        break
+
+
+def scrape(url_id):
+
+    check_url = "https://portal.eks.sk/SpravaZakaziek/Zakazky/Detail/{}".format(url_id)
+    read_url = request.urlopen(check_url)
+    check_soup = BeautifulSoup(read_url.read(), "html.parser", from_encoding="utf-8")
+    test2 = check_soup.find(id="Zakazka_Nazov")
+    zak_nazov = re.search("value=\"(.*)\s*\"", str(test2)).group(1)
+
+    l = check_soup(id="txbPredpokladanaCenaPlnenia")
+    zak_cena = re.search("value=\"([\d]+),\d{2}", str(l)).group(1)
+
+    m = check_soup(id="Zakazka_Identifikator")
+    zak_id = re.search("value=\"(.*)\"", str(m)).group(1)
+
+    n = check_soup(id="Zakazka_LehotaNaPredkladaniePonuk")
+    zak_lehota_ponuky = re.search("value=\"(.*)\"", str(n)).group(1)
+
+    CPV = check_soup(id="cpvData")
+    zak_CPV = re.findall("kod\" \:\"(\d{8}-\d+)", str(CPV))
+    CPV_2digs = re.findall("kod\" \:\"(\d{2})", str(CPV))
+    dataset = {url_id:{
+               "Show": False,
+               "details": {
+                           "ID_zakazky": zak_id,
+                           "url": check_url,
+                           "nazov": zak_nazov,
+                           "CPV": zak_CPV,
+                           "vyska zdrojov": zak_cena,
+                           "termin ponuky": zak_lehota_ponuky
+                           }}}
+
+    intersect = []
+    for item in config['DEFAULT']['CPV_starts_with'].split(','):        # match CPV
+        for member in CPV_2digs:
+            if item == member:
+                intersect.append(item)
+
+    if int(zak_cena) > int(config['DEFAULT']['zak_cena']) and len(intersect) > 0:
+        dataset[url_id]['Show'] = True
+        with open("EKS/Data/{}".format(url_id), "w+", encoding="utf-8") as f:
+            json.dump(dataset, f, sort_keys=True, indent=0)
+
+
+def run():
+    log_file = open('EKS/eks.log', 'a+')
+    for x in range(max(file_list)-2, max(file_list)+2):
+        try:
+            log_file.write(str(datetime.now()) + ' : ' + "Extracting data for ID: {}\n".format(x))
+            if x not in file_list:
+                scrape(x)
+                file_list.insert(0, x)
+                log_file.write(str(datetime.now()) + ' : ' + "Inserting data to list.json: {}\n".format(x))
+            else:
+                log_file.write(str(datetime.now()) + ' : ' + "{} uz spracovane\n".format(x))
+        except AttributeError:
+            log_file.write(str(datetime.now()) + ' : ' + "Funkcia pre ID {} zlyhala\n".format(x))
+            pass
+        with open("EKS/list.json", "w+") as y:
+            json.dump(file_list, y)
+        sleep(10)
+    while os.listdir('EKS/Data'):
+        #sleep(3)
+        smtp.send_mail()
+        item = min(os.listdir('EKS/Data'))
+        os.rename('EKS/Data/'+item, 'EKS/Data_processed/'+item)
+        log_file.write(str(datetime.now()) + ' : '+item+' processed\n')
+
+    smtp.smtpserver.close()
+    log_file.close()
---
+++ EKS/smtp.py
@@ -0,0 +1,94 @@
+__author__ = 'TH'
+
+# Import smtplib for the actual sending function
+import smtplib, json, os
+from datetime import datetime
+from configparser import ConfigParser
+
+# Import the email modules we'll need
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+config = ConfigParser()
+config.read("settings.ini")
+
+# Open a plain text file for reading.  For this example, assume that
+# the text file contains only ASCII characters.
+
+
+def read_data():
+
+    file = min(os.listdir('EKS/Data'))
+    f = open('EKS/Data/' + file)
+
+    x = json.load(f)[file]['details']
+    ID_zakazky = x['ID_zakazky']
+    url = x['url']
+    nazov = x['nazov']
+    CPV= x['CPV']
+    vyska_zdrojov = x['vyska zdrojov']+'€'
+    termin_ponuky = x['termin ponuky']
+
+    return [ID_zakazky, nazov, CPV, vyska_zdrojov, termin_ponuky, url]
+
+
+# Create the body of the message (a plain-text and an HTML version).
+
+to = config['DEFAULT']['recipients'].split(',')
+
+login_user = config['DEFAULT']['login_user']
+login_user_pwd = config['DEFAULT']['login_user_pwd']
+smtpserver = smtplib.SMTP(config['DEFAULT']['smtp_server'])
+
+
+def send_mail():
+    smtpserver.login(login_user, login_user_pwd)
+
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = read_data()[0] + ', ponuka do: ' + read_data()[4][:10]
+    msg['From'] = 'EKS Portal'
+    msg['To'] = to[0]
+
+    text = str(read_data())
+    part1 = MIMEText(text, 'plain')
+
+    html = """\
+    <html>
+      <head></head>
+      <body>
+        <table border="1" cellpadding="2" cellspacing="2">
+          <tbody>
+            <tr>
+              <td valign="top" align="center">ID zákazky<br></a></td>
+              <td valign="top" align="center">názov<br></td>
+              <td valign="top" align="center">CPV<br></td>
+              <td valign="top" align="center">Výška zdrojov<br></td>
+              <td valign="top" align="center">Termín ponuky</td>
+            </tr>
+              <tr>
+              <td valign="top" align="left"><a href={url}>{ID_zakazky}<br></a></td>
+              <td valign="top" align="left">{nazov}<br></td>
+              <td valign="top" align="left">{CPV}<br></td>
+              <td valign="top" align="left">{vyska_zdrojov}<br></td>
+              <td valign="top" align="left">{termin_ponuky}</td>
+              </tr>
+      </body>
+    </html>
+    """.format(url=read_data()[5],
+               ID_zakazky=read_data()[0],
+               nazov=read_data()[1],
+               CPV=read_data()[2],
+               vyska_zdrojov=read_data()[3],
+               termin_ponuky=read_data()[4])
+    part2 = MIMEText(html, 'html')
+
+    msg.attach(part1)
+    msg.attach(part2)
+
+    smtpserver.sendmail(login_user, to, msg.as_string())
+    item = min(os.listdir('EKS/Data'))
+    log_file = open('EKS/eks.log', 'a')
+    log_file.write(str(datetime.now()) + ' : ' + item + ' sent to:'+str(to)+'\n')
+    log_file.close()
+    #smtpserver.close()
+
